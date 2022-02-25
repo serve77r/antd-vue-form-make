6c9bd7265e0f8333cc19fa54c263731ee28d40b8
nick.huang@sjfood.com <1931130013@qq.com>
2022-02-24 19:09:35 -0800

    add form make

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FormMake from 'antd-vue-form-make'
+import 'antd-vue-form-make/packageDist/antd-vue-form-make.css'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 Vue.config.productionTip = false
+
+Vue.use(Antd)
+Vue.use(FormMake)
 
 new Vue({
   render: h => h(App),
